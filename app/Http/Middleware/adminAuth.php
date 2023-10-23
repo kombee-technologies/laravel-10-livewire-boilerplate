5b57836e4c5a6e6b71c6f3e18613013a63c4d68c
Jenish Prajapati <jenish.prajapati@easternts.com>
2023-10-23 12:19:23 +0530

    - Changes in kernel.php file

--- app/Http/Middleware/adminAuth.php
+++ app/Http/Middleware/adminAuth.php
@@ -7,7 +7,7 @@ use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Illuminate\Support\Facades\Auth;
 
-class adminAuth
+class AdminAuth
 {
     /**
      * Handle an incoming request.
